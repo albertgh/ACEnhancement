3091fc2d3e9c0ae0dcbd9d583cf23ba8e1b8d3f6
Albert Chu <albertzyk@gmail.com>
2020-12-21 16:38:45 +0800

    loading support msg
    msg hud

--- Example/ACEnhancementExample-iOS/ViewController.swift
+++ Example/ACEnhancementExample-iOS/ViewController.swift
@@ -15,22 +15,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        ace_delay(by: 0.5) {
+        UIWindow.ace.showLoading(darkStyle: true)
+        
+        ace_delay(by: 3.5) {
+            UIWindow.ace.showLoading(message: "正在测试...",
+                                     darkStyle: true)
+        }
+        
+        ace_delay(by: 8.5) {
             
-            UIAlertController.ace.showAlert(
-                inVC: self,
-                darkStyle: true,
-                titleText: "test",
-                messageText: "some msg",
-                cancelTuple: ("cancel", { (action) in
-                    debugPrint("cancel")
-                }),
-                normalActionTuples:  [
-                    ("ok", { (action) in
-                        debugPrint("ok")
-                    })
-                ]
-            )
+            UIWindow.ace.showLoading(message: "继续在测试...Created by ac_m1a on 2020/12/20Created by ac_m1a on 2020/12/20Created by ac_m1a on 2020/12/20Created by ac_m1a on 2020/12/20",
+                                     darkStyle: true)
+            debugPrint("check blink")
+
+//            UIAlertController.ace.showAlert(
+//                inVC: self,
+//                darkStyle: true,
+//                titleText: "test",
+//                messageText: "some msg",
+//                cancelTuple: ("cancel", { (action) in
+//                    debugPrint("cancel")
+//                }),
+//                normalActionTuples:  [
+//                    ("ok", { (action) in
+//                        debugPrint("ok")
+//                    })
+//                ]
+//            )
             
         }
     }
--- Sources/ACEnhancement/Extension/UIWindow+ACEnhancement.swift
+++ Sources/ACEnhancement/Extension/UIWindow+ACEnhancement.swift
@@ -28,61 +28,24 @@ extension ACENamespaceWrapper where WrappedType == UIWindow {
         return UIScreen.main.bounds.size
     }()
     
-    public static func showLoading(dimmingAlpha: CGFloat = 0.3,
-                                   yOffset: CGFloat = 0.0,
-                                   darkStyle: Bool = false) {
+    // MARK: common loading hud
+    public static func showLoading(dimmingAlpha: CGFloat? = 0.3,
+                                   yOffset: CGFloat? = 0.0,
+                                   message: String? = nil,
+                                   messageFont: UIFont? = UIFont.systemFont(ofSize: 18.0),
+                                   darkStyle: Bool? = false) {
         DispatchQueue.main.async {
             if let currentWindow: UIWindow = UIApplication.shared.windows.first {
-                if let loadingBG = currentWindow.viewWithTag(UIWindow.ace_loadingBGViewTag()) {
-                    loadingBG.removeFromSuperview()
+                if let existLoadingView = currentWindow.viewWithTag(UIWindow.ace_loadingBGViewTag()) {
+                    //existLoadingView.ace.removeAllSubviews()
+                    existLoadingView.removeFromSuperview()
                 }
                 
-                var indicatorStyle: UIActivityIndicatorView.Style = .whiteLarge
-                var indicatorColor: UIColor = .darkGray
-                var bgEffectStyle: UIBlurEffect.Style = .light
-                if #available(iOS 13, *) {
-                    indicatorStyle = .large
-                    bgEffectStyle = .systemUltraThinMaterialLight
-                }
-                if darkStyle {
-                    indicatorStyle = .whiteLarge
-                    indicatorColor = .white
-                    bgEffectStyle = .dark
-                    if #available(iOS 13, *) {
-                        indicatorStyle = .large
-                        bgEffectStyle = .systemUltraThinMaterialDark
-                    }
-                }
+                let loadingView = UIWindow.ace.assembleLoadingView(dimmingAlpha: dimmingAlpha, yOffset: yOffset, message: message, darkStyle: darkStyle)
+                currentWindow.addSubview(loadingView)
+                loadingView.ace.constraintEdges()
                 
-                let loadingBG = UIView()
-                loadingBG.backgroundColor = UIColor.black.withAlphaComponent(dimmingAlpha)
-                currentWindow.addSubview(loadingBG)
-                loadingBG.isUserInteractionEnabled = true
-                loadingBG.ace.constraintEdges()
-                
-                let loadingContainerViewEdge: CGFloat = 100.0
-                let loadingContainerView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: bgEffectStyle))
-                loadingContainerView.layer.cornerRadius = 8.0
-                loadingContainerView.layer.masksToBounds = true
-                loadingContainerView.clipsToBounds = true
-                loadingBG.addSubview(loadingContainerView)
-                loadingContainerView.ace.constraint(attribute: .centerX)
-                loadingContainerView.ace.constraint(attribute: .centerY, constant: yOffset)
-                loadingContainerView.ace.constraintW(loadingContainerViewEdge)
-                loadingContainerView.ace.constraintH(loadingContainerViewEdge)
-                
-                let loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(style: indicatorStyle)
-                loadingIndicator.backgroundColor = .clear
-                loadingIndicator.color = indicatorColor
-                loadingIndicator.hidesWhenStopped = true
-                loadingContainerView.contentView.addSubview(loadingIndicator)
-                loadingIndicator.ace.constraint(attribute: .centerX)
-                loadingIndicator.ace.constraint(attribute: .centerY)
-                loadingIndicator.ace.constraintW(loadingContainerViewEdge)
-                loadingIndicator.ace.constraintH(loadingContainerViewEdge)
-                loadingIndicator.startAnimating()
-                
-                loadingBG.tag = UIWindow.ace_loadingBGViewTag()
+                loadingView.tag = UIWindow.ace_loadingBGViewTag()
                 currentWindow.ace_loadingBGViewTag = UIWindow.ace_loadingBGViewTag()
             }
         }
@@ -96,6 +59,93 @@ extension ACENamespaceWrapper where WrappedType == UIWindow {
                 }
             }
         }
+    }
+    private static func assembleLoadingView(dimmingAlpha: CGFloat? = 0.3,
+                                            yOffset: CGFloat? = 0.0,
+                                            message: String? = nil,
+                                            messageFont: UIFont? = UIFont.systemFont(ofSize: 18.0),
+                                            darkStyle: Bool? = false) -> UIView {
+        var textColor: UIColor = .black
+        var indicatorStyle: UIActivityIndicatorView.Style = .whiteLarge
+        var indicatorColor: UIColor = .darkGray
+        var bgEffectStyle: UIBlurEffect.Style = .light
+        if #available(iOS 13, *) {
+            indicatorStyle = .large
+            bgEffectStyle = .systemUltraThinMaterialLight
+        }
+        if let dark = darkStyle, dark == true {
+            textColor = .lightText
+            indicatorStyle = .whiteLarge
+            indicatorColor = .white
+            bgEffectStyle = .dark
+            if #available(iOS 13, *) {
+                indicatorStyle = .large
+                bgEffectStyle = .systemUltraThinMaterialDark
+            }
+        }
+        
+        var loadingBGAlpha: CGFloat = 0.3
+        if let dimmingAlpha = dimmingAlpha {
+            loadingBGAlpha = dimmingAlpha
+        }
+        let loadingBG = UIView()
+        loadingBG.backgroundColor = UIColor.black.withAlphaComponent(loadingBGAlpha)
+        loadingBG.isUserInteractionEnabled = true
+        
+        var loadingContainerViewYOffset: CGFloat = 0.0
+        if let loadingYOffset = yOffset {
+            loadingContainerViewYOffset = loadingYOffset
+        }
+        var loadingContainerViewW: CGFloat = 100.0
+        var loadingContainerViewH: CGFloat = 100.0
+        let loadingContainerView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: bgEffectStyle))
+        loadingContainerView.layer.cornerRadius = 8.0
+        loadingContainerView.layer.masksToBounds = true
+        loadingContainerView.clipsToBounds = true
+        loadingBG.addSubview(loadingContainerView)
+        if let msg = message, msg.isEmpty == false {
+            let gapSpace: CGFloat = 10.0
+            loadingContainerViewW = 260.0
+            loadingContainerViewH = 200.0
+            let messageTextView: UITextView = UITextView()
+            messageTextView.isEditable = false
+            messageTextView.font = messageFont
+            messageTextView.textColor = textColor
+            messageTextView.textAlignment = .center
+            messageTextView.text = message
+            messageTextView.backgroundColor = .clear
+            loadingContainerView.contentView.addSubview(messageTextView)
+            
+            messageTextView.sizeToFit()
+            var tvH: CGFloat = 100.0
+            let tvFullH: CGFloat = CGFloat(ceil(messageTextView.sizeThatFits(CGSize(width: (loadingContainerViewW - gapSpace * 2.0), height: tvH)).height))
+            if tvFullH < 100.0 {
+                loadingContainerViewH -= (tvH - tvFullH)
+                tvH = messageTextView.contentSize.height
+            }
+            messageTextView.ace.constraint(attribute: .left, constant: gapSpace)
+            messageTextView.ace.constraint(attribute: .right, constant: -gapSpace)
+            messageTextView.ace.constraint(attribute: .bottom, constant: -gapSpace)
+            messageTextView.ace.constraintH(tvH)
+        }
+        loadingContainerView.ace.constraint(attribute: .centerX)
+        loadingContainerView.ace.constraint(attribute: .centerY, constant: loadingContainerViewYOffset)
+        loadingContainerView.ace.constraintW(loadingContainerViewW)
+        loadingContainerView.ace.constraintH(loadingContainerViewH)
+        
+        let loadingIndicatorEdge: CGFloat = 100.0
+        let loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(style: indicatorStyle)
+        loadingIndicator.backgroundColor = .clear
+        loadingIndicator.color = indicatorColor
+        loadingIndicator.hidesWhenStopped = true
+        loadingContainerView.contentView.addSubview(loadingIndicator)
+        loadingIndicator.ace.constraint(attribute: .centerX)
+        loadingIndicator.ace.constraint(attribute: .top, constant: 0)
+        loadingIndicator.ace.constraintW(loadingIndicatorEdge)
+        loadingIndicator.ace.constraintH(loadingIndicatorEdge)
+        loadingIndicator.startAnimating()
+        
+        return loadingBG
     }
 }
 
