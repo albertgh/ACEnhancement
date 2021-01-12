087138b191636cd8d2e65db38862524f0032051f
Albert Chu <albertzyk@gmail.com>
2021-01-12 19:03:38 +0800

    fix comp

--- Sources/ACEnhancement/Extension/UIImage+ACEnhancement.swift
+++ Sources/ACEnhancement/Extension/UIImage+ACEnhancement.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-extension ACENamespaceWrapper where WrappedType == UIImage? {
+extension ACENamespaceWrapper where WrappedType == UIImage {
     
     public func tint(_ color: UIColor,
-                     blendMode: CGBlendMode = .destinationIn) -> UIImage {
+                     blendMode: CGBlendMode = .destinationIn) -> UIImage? {
         let `self` = wrappedValue
         
         UIGraphicsBeginImageContextWithOptions(self.size, false, 0.0)
@@ -18,10 +18,10 @@ extension ACENamespaceWrapper where WrappedType == UIImage? {
         let bounds: CGRect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
         UIRectFill(bounds)
         
-        draw(in: bounds, blendMode: blendMode, alpha: 1.0)
+        self.draw(in: bounds, blendMode: blendMode, alpha: 1.0)
         
         if blendMode != .destinationIn {
-            draw(in: bounds, blendMode: .destinationIn, alpha: 1.0)
+            self.draw(in: bounds, blendMode: .destinationIn, alpha: 1.0)
         }
         
         let tinted: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
