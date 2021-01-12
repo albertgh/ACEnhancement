918ddf8802e4ed5c5048f375816ab0211897af5c
Albert Chu <albertzyk@gmail.com>
2021-01-12 18:56:45 +0800

    add tint for os below iOS 13

--- Sources/ACEnhancement/Extension/UIImage+ACEnhancement.swift
+++ Sources/ACEnhancement/Extension/UIImage+ACEnhancement.swift
@@ -7,7 +7,29 @@
 
 import UIKit
 
-extension ACENamespaceWrapper where WrappedType == UIImage {
+extension ACENamespaceWrapper where WrappedType == UIImage? {
+    
+    public func tint(_ color: UIColor,
+                     blendMode: CGBlendMode = .destinationIn) -> UIImage {
+        let `self` = wrappedValue
+        
+        UIGraphicsBeginImageContextWithOptions(self.size, false, 0.0)
+        color.setFill()
+        let bounds: CGRect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        UIRectFill(bounds)
+        
+        draw(in: bounds, blendMode: blendMode, alpha: 1.0)
+        
+        if blendMode != .destinationIn {
+            draw(in: bounds, blendMode: .destinationIn, alpha: 1.0)
+        }
+        
+        let tinted: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return tinted
+    }
+    
     public func cgImageWithFixedOrientation() -> CGImage? {
         let `self` = wrappedValue
         
